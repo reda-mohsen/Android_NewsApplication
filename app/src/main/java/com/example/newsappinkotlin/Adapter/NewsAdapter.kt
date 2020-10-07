c2cc18efff164e1a620aaa6060d8e386c44a14fd
amr <w_wla@yahoo.com>
2020-10-08 01:33:27 +0200

    Commit: Updating adapter for API usage

--- app/src/main/java/com/example/newsappinkotlin/Adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/newsappinkotlin/Adapter/NewsAdapter.kt
@@ -6,31 +6,28 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsappinkotlin.Model.News
 import com.example.newsappinkotlin.R
+import kotlinx.android.synthetic.main.card.view.*
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 
-class NewsAdapter (var news:MutableList<News>): RecyclerView.Adapter<NewsAdapter.MyVH>() {
+
+class NewsAdapter(var news:MutableList<News>): RecyclerView.Adapter<NewsAdapter.MyVH>() {
     class MyVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun onbind(n: News){
-            //itemView.txt.text=(title)
-            //itemView.txt2.text=(url)
+        fun onbind(n:News){
+            itemView.txt.text=n.title
+            itemView.txt2.text=n.author
+            Glide.with(itemView).load("${n.image}").transform(CenterCrop()).into(itemView.img)
         }
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVH =
-        MyVH(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.card,
-                parent,
-                false
-            )
-        )
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVH=MyVH(
+        LayoutInflater.from(parent.context).inflate(R.layout.card,parent, false)
+    )
     override fun getItemCount(): Int =news.size
 
     override fun onBindViewHolder(holder: MyVH, position: Int) {
         holder.onbind(news[position])
     }
-
-    public fun append(list: List<News>){
+    fun append(list: List<News>){
         this.news.addAll(list)
         notifyItemRangeChanged(this.itemCount,news.size-1)
     }
