33e809e0439a7377e5f101604fc35f8bc57e974c
amr <w_wla@yahoo.com>
2020-10-08 00:21:57 +0200

    Second Commit: Made RecyclerView

---
+++ app/src/main/java/com/example/newsappinkotlin/NewsAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.newsappinkotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter (var news:MutableList<News>): RecyclerView.Adapter<NewsAdapter.MyVH>() {
+    class MyVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun onbind(n:News){
+            //itemView.txt.text=(title)
+            //itemView.txt2.text=(url)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVH=MyVH(
+        LayoutInflater.from(parent.context).inflate(R.layout.card,parent, false)
+    )
+
+    override fun getItemCount(): Int =news.size
+
+    override fun onBindViewHolder(holder: MyVH, position: Int) {
+        holder.onbind(news[position])
+    }
+
+    public fun append(list: List<News>){
+        this.news.addAll(list)
+        notifyItemRangeChanged(this.itemCount,news.size-1)
+    }
+
+
+}
